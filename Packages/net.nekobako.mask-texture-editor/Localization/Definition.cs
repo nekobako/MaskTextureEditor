00043c0990aea393a9d0d83749a88e298486c57e
nekobako <nekobako.tofu@gmail.com>
2025-10-02 20:06:50 +0900

    feat: change default language

--- Packages/net.nekobako.mask-texture-editor/Localization/Definition.cs
+++ Packages/net.nekobako.mask-texture-editor/Localization/Definition.cs
@@ -3,6 +3,6 @@ namespace net.nekobako.MaskTextureEditor.Localization
     internal static class Definition
     {
         [CustomLocalization4EditorExtension.AssemblyCL4EELocalization]
-        private static CustomLocalization4EditorExtension.Localization Localization { get; } = new("1b6661244fd2dc646af94ebf766dd8aa", "ja-jp");
+        private static CustomLocalization4EditorExtension.Localization Localization { get; } = new("51f30c7607625f642b2e19fcc4972fc4", "en-us");
     }
 }
